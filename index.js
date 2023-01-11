b60b52d366ab3e1a136f7dcd69c6496f8f29dafa
Keino Chichester <keino.chichester@gmail.com>
2023-01-11 11:55:44 -0500

    submit control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dist) {
+  let result = 0;
+  if (dist <= 400) {
+    return `This one is on me!`;
+  } else if (dist > 400 && dist <= 2000) {
+    return `That will be twenty bucks.`
+  } else if (dist > 2000 && dist <= 2500) {
+  return `I will gladly take your thirty bucks.`
+}
+return `No can do.`;
+}
+console.log(scuberGreetingForFeet(500))
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+    } else if (city !== "NYC") {
+      return "No go."
+    }
+  }
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return `Thank you so much.`
+    } else if (tip === "not as generous") {
+      return `Thank you.`
+  } else 
+    return `Bye.` // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(scuberGreetingForFeet())
+console.log(ternaryCheckCity())
+console.log(switchOnCharmFromTip())
